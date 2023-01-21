56571f57837d985e8eb064039290980c16b95f05
Gibran Dimasagung <gibrandev@gmail.com>
2023-01-21 09:00:39 +0700

    First uploads

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+        name: 'NextJS Apps',
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start -p 3002',
+        instances: 1,
+        watch: false,
+    }]
+};
